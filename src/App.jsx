b1be227135d52dc167a2fdf01adafbac14354ed8
28-ys <lucksharma2017@gmail.com>
2021-06-29 20:10:50 +0530

    media query is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
- 
+import React, { useState, useEffect } from 'react';
+import HashLoader from "react-spinners/HashLoader"; 
 
 
 
 const App= () =>{
+    const[loading, setLoading] = useState(false);
+    useEffect(()=>{
+         setLoading(true)
+         setTimeout(()=>{
+            setLoading(false)
+         },5000);
+    },[])
     let time = new Date().toLocaleTimeString();
     const [ctime, setCtime] = useState(time);
     const updateTime = () =>{
@@ -12,12 +19,24 @@ const App= () =>{
     }
     setInterval(updateTime, 1000);
     return(<>
-        <p className="main-text">
+     {
+         loading ?
+         <HashLoader 
+         color={"#333333"} 
+         loading={loading}  
+         size={150} />
+
+         :
+         <div className="Main">
+         <p className="main-text">
         <span className="slogan">The  
         <b className="spch"> २</b> most <br/>  powerful warriors are <br/> 
         <b className="spch">PATIENCE</b> and <b className="spch">TIME</b></span>
         </p>
         <h1>{ctime}</h1>
-        </>)
+        </div>
+     }
+     </>
+        )
 }
 export default App;
